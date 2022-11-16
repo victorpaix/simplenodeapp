050eb90425b90500c75416ce89aed225c48ec8cf
Victorpaix <35776458+victorpaix@users.noreply.github.com>
2022-11-16 14:49:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-//small change here
+//small change here for test
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
